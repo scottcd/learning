192bcfa225a31af7f10fb19675d33b6e35b7b4c4
Chandler Scott <scottcd1@etsu.edu>
2022-12-30 22:11:17 -0500

    good start

--- react/test-app/src/App.js
+++ react/test-app/src/App.js
@@ -1,15 +1,56 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Two from './pages/Two'
+import Navbar from './components/Navbar'
+import pic from './pic.jpg';
 
-function App() {
-  const name = 'Chandler';
-  const header = (
-    <header class='App-shell'>
-      <h1>site header</h1>
+function AppHeader() {
+  return (
+    <header className='app-header'>
+      <img className='header-image' src={pic}/>
+      <h1>A Fantastic Website</h1>
     </header>
   );
-  const element = <h1>Hello, {name}!</h1>;
-  const footer = <footer class='App-shell'>site footer</footer>;
-  const appShell = <div class='App-shell'>{header} {element} {element} {footer}</div>
+}
+
+
+function AppContent(props) {
+  return (
+    <div className='app-content'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={Home()} exact />
+        <Route path="/two" element={Two()} />
+      </Routes>
+    </div>
+  );
+}
+
+function AppFooter() {
+  return (
+    <footer className='app-footer'>
+      site footer
+    </footer>
+  );
+}
+
+function App() {
+  // header
+  const headerSection = <AppHeader/>;
+  // content
+  const contentSection = <AppContent name='Chandler'/>
+  // footer
+  const footerSection = <AppFooter/>;
+  
+  // combine header, content, footer for app shell
+  const appShell = (
+      <div className='app-shell'>
+        {headerSection}
+        {contentSection}
+        {footerSection}
+      </div>
+    );
 
   return ( appShell );
 }
